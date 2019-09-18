c8ef1fcbb9bfe754c4eb8ddbc804d396c0cbf026
diego programer <diegoalexac@gmail.com>
2019-09-18 13:36:42 -0500

    programa de promedio de notas con vectores en java

---
+++ src/sabado7/Sabado7.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sabado7;
+
+/**
+ *
+ * @author alexander
+ */
+public class Sabado7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       // definicion o declaracion de variables
+       // las de tipo int
+       int dia = 6;
+       int mes = 9;
+       int año = 2019;
+       
+       // en strings
+       String diat ="martes";
+       String mest ="septiembre";
+       String anio ="dosmil diecinueve";
+       
+	// EL dia es
+       System.out.println("dia"+dia+" "+diat);	
+    // EL AÑO ES
+        System.out.println("año "+ año+" "+anio);
+       dia=9;
+    // el mes es 
+       System.out.println("mes"+mes+" "+mest);
+       
+       // imprima dia mes año(solo en numeros)
+       // imprima mes y dia (solo en numeros)
+       // imprima año mes y dia (solo en numeros)
+       
+       //imprima año mes dia(en texto y numeros)
+    }
+    
+}
